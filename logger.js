ba2acbda2a2c1cd19350ae24e4e4c707b6ba6e46
김영관 <kimyogun78@naver.com>
2024-09-24 14:01:25 +0900

    style: 로그 날짜 출력 형식에 시간 정보 추가

--- logger.js
+++ logger.js
@@ -3,11 +3,11 @@ const winston = require("winston");
 const { combine, timestamp, label, printf } = winston.format;
 
 const logFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} ${level}: ${message}`;
+  return `[${timestamp}] ${level}: ${message}`;
 });
 
 const logger = winston.createLogger({
-  format: combine(timestamp({ format: "YYYYMMDD" }), logFormat),
+  format: combine(timestamp({ format: "YYYY-MM-dd HH:mm:ss:SSS" }), logFormat),
   level: "http",
   transports: [new winston.transports.Console()],
 });
