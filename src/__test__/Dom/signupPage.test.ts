104cb499928b392558d1a291786727edf9e34740
김영관 <kimyogun78@naver.com>
2024-10-01 17:28:00 +0900

    test: signup page Header컴포넌트 생성 확인 test

--- src/__test__/Dom/signupPage.test.ts
+++ src/__test__/Dom/signupPage.test.ts
@@ -1,9 +1,9 @@
-// import { header } from "../../component/header.js";
+import { header } from "../../component/header.js";
 
-// const signUpPage: HTMLElement = document.body;
-// describe("Sign up page Test", () => {
-//   it("Sign Up page Header render Test", () => {
-//     signUpPage.innerHTML = header.getTemplate();
-//     expect(signUpPage.querySelector(".navbar")).not.toBe(null);
-//   });
-// });
+const signUpPage: HTMLElement = document.body;
+describe("Sign up page Test", () => {
+  it("Sign Up page Header render Test", () => {
+    signUpPage.innerHTML = header.getTemplate();
+    expect(signUpPage.querySelector(".navbar")).not.toBe(null);
+  });
+});
