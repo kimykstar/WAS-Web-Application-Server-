14ed60c91298dd152b35deb7cc148ae3a544a935
Younggwan <kimyogun78@naver.com>
2024-10-02 18:40:45 +0900

    test: uriProcessor requestParser의 동작 변경에 따른 test코드 변경

--- src/__test__/Server/uriProcessor.test.ts
+++ src/__test__/Server/uriProcessor.test.ts
@@ -3,12 +3,12 @@ import fs from "fs";
 
 describe("URI processor test", () => {
   it.each([
-    ["/index.html", "./src/static/views/index.html", "html"],
-    ["/favicon.ico", "./src/static/images/favicon.ico", "ico"],
-    ["/dog.jpg", "./src/static/images/dog.jpg", "jpg"],
-  ])("Static file data & file extention read test", (uri: string, staticFilePath: string, expExtention: string) => {
-    // const [fileContent, extension] = getResourceAndExtensionByUri(uri);
-    // expect(fileContent).toEqual(fs.readFileSync(staticFilePath));
-    // expect(extension).toBe(expExtention);
+    ["GET", "/views/index.html", "./src/static/views/index.html", "html"],
+    ["GET", "/images/favicon.ico", "./src/static/images/favicon.ico", "ico"],
+    ["GET", "/images/dog.jpg", "./src/static/images/dog.jpg", "jpg"],
+  ])("Static file data & file extention read test", (httpMethod: string, uri: string, staticFilePath: string, expExtention: string) => {
+    const [fileContent, extension] = getResourceAndExtensionByUri(httpMethod, uri, {});
+    expect(fileContent).toEqual(fs.readFileSync(staticFilePath));
+    expect(extension).toBe(expExtention);
   });
 });
