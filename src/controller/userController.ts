652808253e58fa2c3e07c6d5d91388c459cec189
김영관 <kimyogun78@naver.com>
2024-10-07 21:27:19 +0900

    refactor: index.html위치 변경 및 동작 변경

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -12,14 +12,14 @@ class UserController {
   @PostMapping("/create")
   postSignUp({ password, nickName, email }: Record<string, string>) {
     lowdbDao.insertRecord("user", new User(password, nickName, email));
-    return createRedirectionResponse("/user/index.html");
+    return createRedirectionResponse("/index.html");
   }
 
   @PostMapping("/login")
   async login({ email, password }: Record<string, string>) {
     const record = await lowdbDao.getRecord("user", email);
     if (typeof record === "object" && password === record["password"]) {
-      return createLoginRedirectionResponse("/user/index.html");
+      return createLoginRedirectionResponse("/index.html");
     }
     return createRedirectionResponse("/user/login_failed.html");
   }
