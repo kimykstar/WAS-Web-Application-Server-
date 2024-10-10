353d61741cb64004a1ea7e1b72d289344ccd1903
Younggwan <kimyogun78@naver.com>
2024-10-10 16:47:49 +0900

    feat: 세션에 따라 화면 다르게 보이기

--- src/static/javascript/userStatus.js
+++ src/static/javascript/userStatus.js
@@ -2,9 +2,10 @@ export const isLoggedIn = async () => {
   const response = await fetch("/loginCheck", {
     method: "GET",
   });
-
-  if (response.statusCode === 200) {
-    if (response.body === "authorized") return true;
+  const body = await response.text();
+  console.log(body);
+  if (response.status === 200) {
+    if (body === "authorized") return true;
     return false;
   }
   return false;
