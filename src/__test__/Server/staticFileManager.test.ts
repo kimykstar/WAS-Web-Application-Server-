c21ed101684a8aee33689c2f8baa3e5de2fd348c
Younggwan <kimyogun78@naver.com>
2024-10-14 16:59:51 +0900

    test: UriProcessor구조 변경에 따른 테스트 코드 수정

--- src/__test__/Server/staticFileManager.test.ts
+++ src/__test__/Server/staticFileManager.test.ts
@@ -1,40 +1,15 @@
-import { isValidExtension, isExistStaticFile, getStaticFileContent } from "../../server/staticFileManager.ts";
+import { getStaticFileContent } from "../../server/staticFileManager.ts";
 import fs from "fs";
+import Request from "../../server/Request.ts";
 
 describe("Static file manager test", () => {
   it.each([
-    ["images/dog.jpg", true],
-    ["css/index.css", true],
-    ["index.html", true],
-    ["images/favicon.ico", true],
-    ["user/hello.ddd", false],
-    ["user/hello", false],
-    ["user/hello.test.js", false],
-    ["process.test.html", false],
-  ])("isValidExtension func test", (filePath, expectFlag) => {
-    const flag = isValidExtension(filePath);
-    expect(flag).toBe(expectFlag);
-  });
-
-  it.each([
-    ["images/dog.jpg", true],
-    ["css/index.css", true],
-    ["index.html", true],
-    ["images/favicon.ico", true],
-    ["images/cat.png", false],
-    ["process.test.html", false],
-  ])("isExistStaticFile func test", (filePath, expectFlag) => {
-    const flag = isExistStaticFile(filePath);
-    expect(flag).toBe(expectFlag);
-  });
-
-  it.each([
-    ["images/dog.jpg", "./src/static/images/dog.jpg"],
-    ["css/index.css", "./src/static/css/index.css"],
-    ["index.html", "./src/static/index.html"],
-    ["images/favicon.ico", "./src/static/images/favicon.ico"],
-  ])("getStaticFile func test", (fileName, filePath) => {
+    [new Request("GET /images/dog.jpg HTTP/1.1\r\n\r\n"), "./src/static/images/dog.jpg"],
+    [new Request("GET /css/index.css HTTP/1.1\r\n\r\n"), "./src/static/css/index.css"],
+    [new Request("GET / HTTP/1.1\r\n\r\n"), "./src/static/index.html"],
+    [new Request("GET /images/favicon.ico HTTP/1.1\r\n\r\n"), "./src/static/images/favicon.ico"],
+  ])("/getStaticFile func test", (request: Request, filePath) => {
     const fileContent = fs.readFileSync(filePath);
-    expect(getStaticFileContent(fileName)).toEqual(fileContent);
+    expect(getStaticFileContent(request)).toEqual(fileContent);
   });
 });
