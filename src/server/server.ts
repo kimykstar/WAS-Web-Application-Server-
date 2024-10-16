d1d52a2e4c4e218bf74d8df80f752f256a6b21c8
김영관 <kimyogun78@naver.com>
2024-10-16 18:27:00 +0900

    feat: 패킷 잘림 기능 구현

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,26 +5,53 @@ import { createClientErrorResponse } from "./components/helper/responseCreator.t
 import { HttpException } from "../exception/HttpException.ts";
 import Request from "./httpDomain/Request.ts";
 import "../controller/userController.ts";
+import "../controller/postingController.ts";
 
 export const server = net.createServer((socket: any) => {
+  let reqeustBuffer = Buffer.alloc(0);
+  let contentLength: number | null = null;
+  let headerParsed = false;
+
   // chunk를 모아서 end에서 처리하도록
-  socket.on("data", async (data: string) => {
-    const requestText = data.toString();
-    logger.http(requestText);
-    const request = new Request(requestText);
-    try {
-      const response = await getResponseByUri(request);
-      socket.write(response);
-      socket.end();
-    } catch (e: any) {
-      if (e instanceof HttpException) {
-        const response = createClientErrorResponse(e.getStatusCode(), e.message);
+  socket.on("data", async (data: Buffer) => {
+    reqeustBuffer = Buffer.concat([reqeustBuffer, data]);
+
+    if (!headerParsed) {
+      logger.http(data.toString());
+      const headerEndIndex = reqeustBuffer.indexOf("\r\n\r\n");
+      if (headerEndIndex !== -1) {
+        const headers = reqeustBuffer.subarray(0, headerEndIndex).toString();
+        const match = headers.match(/Content-Length:\s*(\d+)/i);
+        contentLength = match ? parseInt(match[1], 10) : null;
+      }
+      headerParsed = true;
+    }
+
+    if (
+      headerParsed &&
+      (contentLength === null || reqeustBuffer.length >= contentLength + reqeustBuffer.indexOf("\r\n\r\n") + 4)
+    ) {
+      const requestText = reqeustBuffer.toString();
+      logger.http(requestText);
+      try {
+        const request = new Request(requestText);
+        const response = await getResponseByUri(request);
         socket.write(response);
         socket.end();
-        return;
-      }
+      } catch (e: any) {
+        if (e instanceof HttpException) {
+          const response = createClientErrorResponse(e.getStatusCode(), e.message);
+          socket.write(response);
+          socket.end();
+          return;
+        }
 
-      throw e;
+        throw e;
+      } finally {
+        reqeustBuffer = Buffer.alloc(0);
+        contentLength = null;
+        headerParsed = false;
+      }
     }
   });
 });
