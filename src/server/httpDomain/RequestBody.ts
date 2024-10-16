cc8b36f9d29da06b78b183bd95a6f1a9c1320990
Younggwan <kimyogun78@naver.com>
2024-10-16 23:37:49 +0900

    style: 주석 제거

--- src/server/httpDomain/RequestBody.ts
+++ src/server/httpDomain/RequestBody.ts
@@ -4,12 +4,7 @@ export default class RequestBody {
   private bodyContent: Array<Record<string, string>> = [];
 
   parseMultipartBody(boundary: string, body: string) {
-    const contents = body.split(boundary).slice(1, -1);
-    // 각 부분들을 parsing하는 메소드 구현
     const parts = this.splitParts(boundary, body);
-    // parsing된 각 부분들을 순회하면서 각 part를 parsing하고 bodyContent에 append한다.
-    // part를 parsing하려면 header와 body를 CRLF두번을 indexOf를 활용해서 분리한다.
-    // body의 경우에는 data를 키로하고, header는 정규표현식으로 추출해서 추출한다.
 
     const result = this.parseParts(parts.slice(1, -1));
     this.bodyContent = result;
